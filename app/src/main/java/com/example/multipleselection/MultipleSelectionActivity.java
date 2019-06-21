6928e2f44bd2418a393d287e1e369b5bfa6892e7
=Ajay <=ajay122011@gmail.com>
2019-06-21 17:19:49 +0530

    test commit

---
+++ app/src/main/java/com/example/multipleselection/MultipleSelectionActivity.java
@@ -0,0 +1,65 @@
+package com.example.multipleselection;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+public class MultipleSelectionActivity extends AppCompatActivity {
+
+    private android.support.v7.widget.RecyclerView recyclerView;
+    private ArrayList<Employee> employees = new ArrayList<>();
+    private MultiAdapter adapter;
+    private android.support.v7.widget.AppCompatButton btnGetSelected;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.btnGetSelected = (AppCompatButton) findViewById(R.id.btnGetSelected);
+        this.recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        getSupportActionBar().setTitle("Multiple Selection");
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        adapter = new MultiAdapter(this, employees);
+        recyclerView.setAdapter(adapter);
+
+        createList();
+
+        btnGetSelected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (adapter.getSelected().size() > 0) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (int i = 0; i < adapter.getSelected().size(); i++) {
+                        stringBuilder.append(adapter.getSelected().get(i).getName());
+                        stringBuilder.append("\n");
+                    }
+                    showToast(stringBuilder.toString());
+                } else {
+                    showToast("No Selection");
+                }
+            }
+        });
+    }
+
+    private void createList() {
+        employees = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Employee employee = new Employee();
+            employee.setName("Employee " + (i + 1));
+            employees.add(employee);
+        }
+        adapter.setEmployees(employees);
+    }
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
